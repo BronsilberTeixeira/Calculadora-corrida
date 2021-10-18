b957555748352d79ffa19c71fd0b40d20fd06fa8
bron.teixeira.s@gmail.com <bron.teixeira.s@gmail.com>
2021-10-18 09:48:34 -0300

    JS realizado

--- js.js
+++ js.js
@@ -9,5 +9,20 @@ const calcular = document.getElementById('calcular')
 
         if(nome !== '' && peso !== '' && distancia !== '' && tempo !==''){
             const tempo_corte = tempo.split(':')
+
+            const horas = parseInt(tempo_corte[0]);
+            const minutos = parseInt(tempo_corte[1]);
+            const segundos = parseInt(tempo_corte[2]);
+
+            const tempo_horas = horas + (minutos/60) + (segundos/3600);
+
+            const tempo_min = (horas*60) + minutos + (segundos/60)
+
+            const velocidade = (distancia/(tempo_horas)).toFixed(2);
+
+            resultado.textContent = `Ola, ${nome} ! você percorreu ${distancia}km em ${tempo}, e teve uma velocidade média de ${velocidade}`
+        }
+        else{
+            resultado.textContent = 'Preencha os campos corretamente para que o calculo seja feito'
         }
     }
