980cf125c47e65c54ebc42cdc6a3e3f5d8b9ace2
bron.teixeira.s@gmail.com <bron.teixeira.s@gmail.com>
2021-10-18 09:18:13 -0300

    Inicio do JS e pequenas alterações do css

---
+++ js.js
@@ -0,0 +1,13 @@
+const calcular = document.getElementById('calcular')
+
+    function pace(){
+        const nome = document.getElementById('nome').value;
+        const peso = document.getElementById('peso').value;
+        const distancia = document.getElementById('distancia').value;
+        const tempo = document.getElementById('tempo').value;
+        const resultado = document.getElementById('resultado');
+
+        if(nome !== '' && peso !== '' && distancia !== '' && tempo !==''){
+            const tempo_corte = tempo.split(':')
+        }
+    }
